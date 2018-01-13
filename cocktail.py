97f403b0659bfd5ff915ff31e82a0edaaefa2d38
kevinorey <kevin.orey@gmail.com>
2018-01-13 10:55:49 -0600

    more work on services

--- cocktail.py
+++ cocktail.py
@@ -7,9 +7,40 @@ class Cocktail:
     def function(self):
         print("Entered Cocktail Constructor")
 
+    def setCockTailId(self, inputId):
+        print("Input Id = ", inputId)
+        if isinstance(inputId, int):
+            print("Input is a number")
+            if inputId > 0:
+                print("Input number is greater than 0")
+                self.cocktailId = inputId
+            else:
+                print("Input number is less than or equal to 0")
+        else:
+            print("Invalid input must be an integer")
+
+                
+                
+##        if inputId.isalpha():
+##            print("Invalid input:  Input must be an integer")
+##        elif inputId.isdigit():
+##            print("Input is a number... checking valid ranges")
+##            if ( inputId > 0 ):
+##                self.cocktailId = inputId
+##            else:
+##                print("Input is less than or equal to 0")
+                
+
+    def getCockTailId(self):
+        return self.cocktailId;
+
 
 def main():
     print("Entered Main")
+    x = Cocktail()
+    print("id = ", x.getCockTailId())
+    x.setCockTailId(0)
+    print("id = ", x.getCockTailId())
     print("Leaving Main")
 
 main()
