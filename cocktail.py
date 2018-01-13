d6822169b7a6935618d2fb75c5c5609927d16326
kevinorey <kevin.orey@gmail.com>
2018-01-13 08:30:41 -0600

    more work on services

---
+++ cocktail.py
@@ -0,0 +1,15 @@
+import json
+
+class Cocktail:
+    cocktailId = 0
+    cocktailName = ""
+
+    def function(self):
+        print("Entered Cocktail Constructor")
+
+
+def main():
+    print("Entered Main")
+    print("Leaving Main")
+
+main()
