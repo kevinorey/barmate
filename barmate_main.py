f9b7b2f2c4204efd49cb9785ae943fa2db2ae961
kevinorey <kevin.orey@gmail.com>
2018-01-17 20:15:27 -0600

    more work on services

---
+++ barmate_main.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+
+        #Create view liquor button
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "View"
+        self.hi_there["command"] = self.viewLiquor
+        self.hi_there.pack(side="top")
+
+        #Create add liquor button
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Add"
+        self.hi_there["command"] = self.addLiquor
+        self.hi_there.pack(side="top")
+
+        self.QUIT = tk.Button(self, text="Quit", fg="red",
+                                            command=root.destroy)
+        self.QUIT.pack(side="bottom")
+
+    def viewLiquor(self):
+        print("Entered View Liquor")
+
+    def addLiquor(self):
+        print("Entered Add Liquor")
+
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
